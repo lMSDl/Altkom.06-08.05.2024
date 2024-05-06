e6af5def889bac23ca313434f064a171ac211c7c
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-06 11:48:45 +0200

    Nullable

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,6 +11,24 @@
             //Jest to zawsze punkt startowy programu
             //Dopuszczalny jest tylko jeden plik w rpojekcie z instrukcjami najwyższego poziomu
             Console.WriteLine("Hello, World!");
-        //}
-    //}
+
+
+            //? - obiket nullowalny - skrót od Nullable<int>
+            int? a = 4;
+            a = null;
+
+            //flaga Nullable z konfiguracji powoduje, że wszystkie typy obiektów będą tak samo traktowane względem null
+            object? b = new object();
+            b = SetValue();
+
+            if (b != null)
+                Console.WriteLine(b.ToString());
+
+
+            static object? SetValue()
+            {
+                return null;
+            }
+//}
+//}
 //}
