d4f492d8ec2428cc09318574b6e4c32330c9db3a
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-06 11:18:43 +0200

    Instrukcje najwyższego poziomu

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace ConsoleApp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+//namespace ConsoleApp
+//{
+    //internal class Program
+    //{
+        //static void Main(string[] args)
+        //{
+
+            //Instrukcja najwyższego poziomu - instrukcja napisana bezpośrednio w pliku bez klasy i namespace
+            //Jest to zawsze punkt startowy programu
+            //Dopuszczalny jest tylko jeden plik w rpojekcie z instrukcjami najwyższego poziomu
             Console.WriteLine("Hello, World!");
-        }
-    }
-}
+        //}
+    //}
+//}
