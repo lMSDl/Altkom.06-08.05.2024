fd24ead2a39a0cab7cfea6db37362dc7cc3b2cca
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-07 09:48:45 +0200

    try-catch

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Models;
 using Services.InMemory;
+using System;
 
 ProductsService _service = new ProductsService();
 
@@ -72,7 +73,7 @@ void Show()
 
 void Create()
 {
-    Product product = new Product
+    Product product = new Product()
     {
         Name = GetString("Name"),
         Price = GetFloat("Price"),
@@ -89,16 +90,51 @@ string GetString(string label)
     return data;
 }
 
-int GetInt(string label)
-{
-    string data = GetString(label);
-    return int.Parse(data);
-}
+
 
 float GetFloat(string label)
 {
     string data = GetString(label);
-    return float.Parse(data);
+
+    //try-catch - służy do obsługi wyjątków.
+    //W bloku try umieszczamy kod w którym może potencjalnie wystąpić wyjątek
+    //Blok catch zawiera kod, który ma być wykonany jeśli wyjątek wystąpi
+    try
+    {
+        return float.Parse(data);
+    }
+    //catch - bez parametru - przechwytuje wszystkie wyjątki i nie daje wglądu w obiekt wyjątku
+    //catch() - z parametrem - przechwytuje wyjątki zgodne z klasą parametru, dając wgląd w obiekt
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+        return GetFloat(label);
+    }
+}
+
+int GetInt(string label)
+{
+    string data = GetString(label);
+    try
+    {
+        //throw new ArgumentException();
+        return int.Parse(data);
+    }
+    //możemy mieć wiele bloków catch w kolejność od szczegółu do ogółu (najbardziej ogólny wyjątek na końcu)
+    //dzięki temu możemy wykonywać różne akcje w zależności od klasy wyjątku
+    catch (IndexOutOfRangeException) //nazwa parametru jest opcjonalna - jeśli nie potrzebujemy odczytywać właściwości z obiektu wyjątku nie musimy deklarować nazwy 
+    {
+        return -1;
+    }
+    catch (FormatException e) //korzystamy z właściwości wyjątku, więc nazywamy jego obiekt jako "e", żeby mieć do niego dostęp
+    {
+        Console.WriteLine(e.Message);
+        return GetInt(label);
+    }
+    catch (Exception)
+    {
+        return -2;
+    }
 }
 
 DateTime GetDateTime(string label)
