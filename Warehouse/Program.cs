c7ddfe15c89e5720572cb34341843e14bcd7bb44
Paweł Łęczycki <pawel@msd.info.pl>
2024-05-07 15:03:07 +0200

    Lambda

--- Warehouse/Program.cs
+++ Warehouse/Program.cs
@@ -1,20 +1,21 @@
 ﻿using Models;
-using Services.InMemory;
 using Warehouse;
 
 
-Pet CreateNew()
-{
-    return new Pet { Name = GenericProgram<Pet>.GetString("Name"), Age = GenericProgram<Pet>.GetInt("Age") };
-}
-
-Pet CreateUpdate(Pet old)
-{
-    return new Pet { Name = GenericProgram<Pet>.GetString($"Name ({old.Name})"), Age = GenericProgram<Pet>.GetInt($"Age ({old.Age})") };
-}
-
 //wykorzystanie dziedziczenia i generyczności
-GenericProgram<Pet> program = new DelegateProgram<Pet>(CreateNew, CreateUpdate);
+//stosujmeny wyrażenia lambda do utworzenia wskaźników na funkcje anonimowe (czyli takie, które nie posiadają nazw)
+GenericProgram<Pet> program = new DelegateProgram<Pet>(
+    () =>
+        new Pet
+        {
+            Name = GenericProgram<Pet>.GetString("Name"),
+            Age = GenericProgram<Pet>.GetInt("Age")
+        },
+    old => new Pet
+    {
+        Name = GenericProgram<Pet>.GetString($"Name ({old.Name})"),
+        Age = GenericProgram<Pet>.GetInt($"Age ({old.Age})")
+    });
 
 program.Run();
 
